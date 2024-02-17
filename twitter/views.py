df595a4265ab222065e9beaa53166d1c78b43639
Hossein <hossienbehzadi1381@gmail.com>
2024-02-17 14:10:29 +0330

    adding profile follows list and follow/unfollow button

--- twitter/views.py
+++ twitter/views.py
@@ -14,3 +14,26 @@ def profile_list(request):
     else:
         messages.success(request, 'You must be logged in to access this page')
         return redirect('home')
+
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+
+        if request.method == "POST":
+            current_user_profile = request.user.profile
+            action = request.POST['follow']
+
+            if action == "unfollow":
+                current_user_profile.follows.remove(profile)
+
+            elif action == "follow":
+                current_user_profile.follows.add(profile)
+
+            current_user_profile.save()
+
+        return render(request, 'profile.html', {'profile': profile})
+
+    else:
+        messages.success(request, 'You must be logged in to access this page')
+        return redirect('home')
